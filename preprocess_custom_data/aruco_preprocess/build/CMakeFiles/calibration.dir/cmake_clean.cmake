713ce9e44113d7ad92d9b316098d15c484b7bc01
tnd79bk <ngocductrann@gmail.com>
2023-05-10 08:52:11 +0700

    commit

---
+++ preprocess_custom_data/aruco_preprocess/build/CMakeFiles/calibration.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/calibration.dir/calibration.cpp.obj"
+  "CMakeFiles/calibration.dir/calibration.cpp.obj.d"
+  "calibration.exe"
+  "calibration.exe.manifest"
+  "calibration.pdb"
+  "libcalibration.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/calibration.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
