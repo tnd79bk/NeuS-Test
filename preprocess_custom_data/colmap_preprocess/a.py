8730f9506bab5e3ec2be86b9640237700328d8a1
tnd79bk <ngocductrann@gmail.com>
2023-05-18 16:22:49 +0700

    a

--- preprocess_custom_data/colmap_preprocess/a.py
+++ preprocess_custom_data/colmap_preprocess/a.py
@@ -25,9 +25,9 @@
 
 import numpy as np
 from PIL import Image
-img = np.zeros([100,100,3],dtype=np.uint8)
+img = np.zeros([800,800,3],dtype=np.uint8)
 img.fill(255) # numpy array!
 im = Image.fromarray(img) #convert numpy array to image
 for i in range(0,100):
-    im.save('../../data/custom-data/data-duc/lego/mask/%3d.jpg'%i)
+    im.save("../../data/custom-data/data-duc/lego/mask/"+f"{i:03}"+".jpg")
 
