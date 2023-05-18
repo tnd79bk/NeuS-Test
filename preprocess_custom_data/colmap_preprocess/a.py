15bf1a90c124401c4b7fef5084337fa68c7e5644
tnd79bk <ngocductrann@gmail.com>
2023-05-18 16:18:07 +0700

    a

--- preprocess_custom_data/colmap_preprocess/a.py
+++ preprocess_custom_data/colmap_preprocess/a.py
@@ -1,24 +1,33 @@
-import os
+# import os
 
-folder_path = "../../data/custom-data/data-duc/lego/images"  # Replace with the path to your folder
+# folder_path = "../../data/custom-data/data-duc/lego/images"  # Replace with the path to your folder
 
-# Get a list of files in the folder
-files = os.listdir(folder_path)
+# # Get a list of files in the folder
+# files = os.listdir(folder_path)
 
-# Sort the files in alphanumeric order
-files.sort()
+# # Sort the files in alphanumeric order
+# files.sort()
 
-# Iterate over the files and rename them
-for i, file_name in enumerate(files):
-    # Get the file extension
-    file_ext = os.path.splitext(file_name)[1]
+# # Iterate over the files and rename them
+# for i, file_name in enumerate(files):
+#     # Get the file extension
+#     file_ext = os.path.splitext(file_name)[1]
     
-    # Create the new file name with zero-padding
-    new_file_name = "{:03d}{}".format(i+1, file_ext)
+#     # Create the new file name with zero-padding
+#     new_file_name = "{:03d}{}".format(i+1, file_ext)
     
-    # Build the full paths to the old and new file names
-    old_file_path = os.path.join(folder_path, file_name)
-    new_file_path = os.path.join(folder_path, new_file_name)
+#     # Build the full paths to the old and new file names
+#     old_file_path = os.path.join(folder_path, file_name)
+#     new_file_path = os.path.join(folder_path, new_file_name)
     
-    # Rename the file
-    os.rename(old_file_path, new_file_path)
+#     # Rename the file
+#     os.rename(old_file_path, new_file_path)
+
+import numpy as np
+from PIL import Image
+img = np.zeros([100,100,3],dtype=np.uint8)
+img.fill(255) # numpy array!
+im = Image.fromarray(img) #convert numpy array to image
+for i in range(0,100):
+    im.save('../../data/custom-data/data-duc/lego/mask/%3d.jpg'%i)
+
